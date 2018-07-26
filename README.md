e379f540cc6dbd1dc834420aa39963a955c676a7
Elley <elley1112@livemail.tw>
2018-07-26 18:44:51 +0800

    First executable version with how to use

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # csv-differ
-Copy and paste your csv file (with line break) to combine or diff them
+
+
+### Requirement
+
+php >= 5.6 (suggest php 7.0)
+Apache
+
+
+### How to use it on mac OS
+
+```
+git clone git@github.com:elleyyu/csv-differ.git
+cd csv-differ/
+php -S localhost:8000
+```
+
+open http://localhost:8000
+
+
---
+++ compute.php
@@ -0,0 +1,52 @@
+<?php
+header('Content-type: application/json');
+
+$lists = $_POST['lists'] ?? [];
+$requestString = trim($_POST['requestString']) ?? '';
+$requestString = str_replace('∩', '&', $requestString);
+$requestString = str_replace('∪', '|', $requestString);
+
+if (empty($requestString) || count($lists) == 0) {
+	echo json_encode([
+		'count' => 0,
+		'text' => "",
+	]);
+	exit;
+}
+$matchesTimes = 0;
+$matchesLists = [];
+$operators = [
+	'&' => 'array_intersect',
+	'-' => 'array_diff',
+	'|' => 'array_merge',
+];
+while( preg_match('/([\$\w\[\]]+)\s*([\&\-\|])\s*(\w+)?/', $requestString, $matches) ) {
+	if (strpos($matches[1], '$matchesLists') === false) {
+		$matchesLists[$matchesTimes] = "{$operators[$matches[2]]}(\$lists['{$matches[1]}'], \$lists['{$matches[3]}'])";
+	} else {
+		$matchesLists[$matchesTimes] = "{$operators[$matches[2]]}({$matches[1]}, \$lists['{$matches[3]}'])";
+	}
+	$requestString = str_replace($matches[0], "\$matchesLists[$matchesTimes]", $requestString);
+
+	$matchesTimes ++;
+}
+
+foreach ($matchesLists as $key => $list) {
+	$now = count($matchesLists) - $key - 1;
+	$requestString = str_replace("\$matchesLists[$now]", $matchesLists[$now], $requestString);
+}
+
+foreach ($lists as $key => $list) {
+	$lists[$list['name']] = explode(PHP_EOL, $list['value']);
+	unset($lists[$key]);
+}
+
+eval("\$result = $requestString;");
+
+echo json_encode([
+	'count' => count($result),
+	'text' => implode(PHP_EOL, $result),
+]);
+exit;
+
+?>
